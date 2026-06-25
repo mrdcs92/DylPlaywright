eecd04d2fed33388e1f77b55437fcf2e78d82ab4
mrdcs92 <mrdcs92@gmail.com>
2026-06-25 14:24:36 -0700

    added grep to config

--- playwright.config.js
+++ playwright.config.js
@@ -13,6 +13,8 @@ import { defineConfig, devices } from '@playwright/test';
  * @see https://playwright.dev/docs/test-configuration
  */
 export default defineConfig({
+  grep:/API/,
+  
   testDir: './tests',
   /* Run tests in files in parallel */
   fullyParallel: true,
